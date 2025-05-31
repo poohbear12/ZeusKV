86b01b5df695460723c8af2acdd24e3a1c9e4c74
ccstar <guangsty@163.com>
2025-05-31 22:24:16 +0800

    feat(log):新增日志记录 - Sl4j + logback

---
+++ redis-server/src/main/java/com/cc/RedisServerLauncher.java
@@ -0,0 +1,17 @@
+package com.cc;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @program: cc-simple-redis
+ * @description: Redis server 启动器
+ * @author: ccstar
+ * @create: 2025-05-31  21:05
+ **/
+@Slf4j
+public class RedisServerLauncher
+{
+    public static void main(String[] args) {
+      log.info("Redis server start!");
+    }
+}
