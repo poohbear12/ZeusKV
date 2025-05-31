53d0c92ddba8dd7d095da093b2cf21ab5c7b2bef
ccstar <guangsty@163.com>
2025-05-31 22:40:54 +0800

    feat(client/common):新增客户端/通用模块 - client(仅框架) - common(仅框架)

---
+++ redis-client/src/main/java/com/cc/RedisClientLauncher.java
@@ -0,0 +1,16 @@
+package com.cc;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @program: cc-simple-redis
+ * @description: Redis client 启动器
+ * @author: ccstar
+ * @create: 2025-05-31  22:04
+**/
+@Slf4j
+public class RedisClientLauncher {
+    public static void main(String[] args) {
+        log.info("Redis client start!");
+    }
+}
